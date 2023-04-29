20ee1914b3a85336c3fbcb9d494747ef6e318bda
Akash Kumar <akash211@gmail.com>
2023-04-29 17:42:52 +0530

    subdag to groups

--- dags/group_dag.py
+++ dags/group_dag.py
@@ -1,36 +1,25 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-
 from datetime import datetime
+from groups.group_downloads import download_tasks
 
-from airflow.operators.subdag import SubDagOperator
-from subdags.subdags_download import subdag_downloads
+with DAG(
+    "group_dag",
+    start_date=datetime(2022, 1, 1),
+    schedule_interval="@daily",
+    catchup=False,
+) as dag:
 
-with DAG('group_dag', start_date=datetime(2022, 1, 1),
-         schedule_interval='@daily', catchup=False) as dag:
+    args = {
+        "start_date": dag.start_date,
+        "schedule_interval": dag.schedule_interval,
+        "catchup": dag.catchup,
+    }
 
-    args = {'start_date': dag.start_date, 'schedule_interval': dag.schedule_interval, 'catchup': dag.catchup}
+    downloads = download_tasks()
 
-    downloads = SubDagOperator(task_id='downloads', subdag=subdag_downloads(dag.dag_id, 'downloads', args))
+    check_files = BashOperator(task_id="check_files", bash_command="sleep 10")
 
-    check_files = BashOperator(
-        task_id='check_files',
-        bash_command='sleep 10'
-    )
 
-    transform_a = BashOperator(
-        task_id='transform_a',
-        bash_command='sleep 10'
-    )
 
-    transform_b = BashOperator(
-        task_id='transform_b',
-        bash_command='sleep 10'
-    )
-
-    transform_c = BashOperator(
-        task_id='transform_c',
-        bash_command='sleep 10'
-    )
-
-    downloads >> check_files >> [transform_a, transform_b, transform_c]
+    downloads >> check_files
